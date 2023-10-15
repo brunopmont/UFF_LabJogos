ce1ef3ff65568514a2428f1354b2c31f2be01c5e
Bruno Porto <bpmonteiro@id.uff.br>
2023-10-15 19:39:09 -0300

    Comentários e organização do pong (finalizado) e Space Invaders (em curso)
    Algumas otimizações da parte estética e redução de bugs nos códigos

--- Pong/pongb - versaocomia.py
+++ Pong/pongb - versaocomia.py
@@ -3,25 +3,25 @@ from PPlay.sprite import *
 from PPlay.gameimage import *
 from PPlay.collision import *
 
-#iniciar janela
+#INCIAR JANELA
 tela = Window(1000, 563)
 teclado = Window.get_keyboard()
 restart = 0
 tela.set_title("Bruno Porto Monteiro")
 ia = 1 
 
-#imagens
-bola = Sprite("png/bola50.png")
-fundo = GameImage("png/onepieceredm.jpg")
-pade = Sprite("png/padbp.png")
-padd = Sprite("png/padbp.png")
+#SPRITES
+bola = Sprite("Pong/png/bola50.png")
+fundo = GameImage("Pong/png/onepieceredm.jpg")
+pade = Sprite("Pong/png/padbp.png")
+padd = Sprite("Pong/png/padbp.png")
 
-#definir posição
+#DEFINIR POSIÇÃO
 pade.set_position(20, tela.height/2-padd.height/2)
 padd.set_position(tela.width - 20 - pade.width, tela.height/2-pade.height/2)
 bola.set_position(tela.width/2-bola.width/2,tela.height/2-bola.height/2)
 
-#variáveis
+#VARIÁVEIS DE VELOCIDADE E PONTUAÇÃO
 velx = 700
 vely = 700
 velpady = 400
@@ -33,7 +33,7 @@ while True:
         bola.move_x(velx* tela.delta_time())
         bola.move_y(vely* tela.delta_time())
 
-    #pontuação e restart
+    #PONTUAÇÃO E CONTROLADOR DE INÍCIO DO JOGO
     if bola.x > tela.width:
         pontosesq += 1
         bola.set_position(tela.width/2-bola.width/2,tela.height/2-bola.height/2)
@@ -45,7 +45,7 @@ while True:
     if teclado.key_pressed("space"):   
         restart = 1
 
-    #movimento bola e colisão com parede
+    #MOVIMENTO DA BOLA E COLISÃO COM O "TETO" E "CHÃO"
     if bola.y < 0 or bola.y + bola.height > tela.height:
         vely = vely * (-1)
         if bola.y < 0:
@@ -53,7 +53,7 @@ while True:
         elif bola.y + bola.height > tela.height:
             bola.y = tela.height - bola.height - 1
 
-    #mov pade com w e a
+    #MOVIMENTO DO PAD ESQUERDO COM TECLAS "W" E "A"
     if teclado.key_pressed("W"):
         if pade.y - 1 > 0:
             pade.move_y(-velpady* tela.delta_time())
@@ -61,7 +61,7 @@ while True:
         if pade.y + pade.height + 1 < tela.height:
             pade.move_y(velpady* tela.delta_time())
 
-    #mov padd com cima e baixo ou ia
+    #MOVIMENTO DO PAD DIREITO COM SETAS PRA CIMA E PRA BAIXO OU COM IA
     if ia == 0:
         if teclado.key_pressed("up"):
             if padd.y - 1 > 0:
@@ -77,7 +77,7 @@ while True:
             if padd.y - 1 > 0:
                 padd.move_y(-400* tela.delta_time())
 
-    #colisão pad e bolinha
+    #COLISÃO DO PAD COM A BOLINHA
     if Collision.perfect_collision(bola, padd):
         velx = -700
     if Collision.perfect_collision(bola, pade):
--- Space Invaders/menu.py
+++ Space Invaders/menu.py
@@ -1,22 +1,24 @@
 from PPlay.window import *
 from PPlay.sprite import *
 
-telanum = 0
+telanum = 0 #VARIÁVEL DE CONTROLE DE QUAL JANELA ESTÁ ABERTA
 janela = Window(500, 500)
 janela.set_title("Space Invaders")
 janela.set_background_color((255, 255, 255))
-cursor = Window.get_mouse()
-teclado = Window.get_keyboard()
+cursor = Window.get_mouse() #ATIVAR MOUSE
+teclado = Window.get_keyboard() #ATIVAR TECLADO
 
-jogar = Sprite("png/jogar.png")
-dificuldade = Sprite("png/dificuldade.png")
-ranking = Sprite("png/ranking.png")
-sair = Sprite("png/sair.png")
+#SPRITES
+jogar = Sprite("Space Invaders/png/jogar.png")
+dificuldade = Sprite("Space Invaders/png/dificuldade.png")
+ranking = Sprite("Space Invaders/png/ranking.png")
+sair = Sprite("Space Invaders/png/sair.png")
 
-facil = Sprite("png/facil.png")
-medio = Sprite("png/medio.png")
-dificil = Sprite("png/dificil.png")
+facil = Sprite("Space Invaders/png/facil.png")
+medio = Sprite("Space Invaders/png/medio.png")
+dificil = Sprite("Space Invaders/png/dificil.png")
 
+#POSIÇÕES DOS SPRITES
 jogar.set_position(janela.width/2-jogar.width/2, janela.height/6)
 dificuldade.set_position(janela.width/2-jogar.width/2, 2 * janela.height/6)
 ranking.set_position(janela.width/2-jogar.width/2, 3 * janela.height/6)
@@ -29,89 +31,90 @@ dificil.set_position(janela.width/2-jogar.width/2, 5 * janela.height/6)
 while True:
     janela.update()
 
-    if telanum == 0:
-    #jogar
-        if cursor.is_over_area([janela.width/2-jogar.width/2, janela.height/6], [janela.width/2+jogar.width/2, janela.height/6+jogar.height]):
-            jogar = Sprite("png/jogar-2.png")
-            jogar.set_position(janela.width/2-jogar.width/2, janela.height/6)
-            if cursor.is_button_pressed(1):
-                janela.clear()
-                telanum = 1
-
-        else:
-            jogar = Sprite("png/jogar.png")
-            jogar.set_position(janela.width/2-jogar.width/2, janela.height/6)
-
-        #dificuldade
-        if cursor.is_over_area([janela.width/2-jogar.width/2, 2*janela.height/6], [janela.width/2+jogar.width/2, 2*janela.height/6+jogar.height]):
-            dificuldade = Sprite("png/dificuldade-2.png")
-            dificuldade.set_position(janela.width/2-jogar.width/2, 2*janela.height/6)
-            if cursor.is_button_pressed(1):
-                janela.clear()
-                telanum = 2
-        else:
-            dificuldade = Sprite("png/dificuldade.png")
-            dificuldade.set_position(janela.width/2-jogar.width/2, 2*janela.height/6) 
-
-        #ranking
-        if cursor.is_over_area([janela.width/2-jogar.width/2, 3*janela.height/6], [janela.width/2+jogar.width/2, 3*janela.height/6+jogar.height]):
-            ranking = Sprite("png/ranking-2.png")
-            ranking.set_position(janela.width/2-jogar.width/2, 3*janela.height/6)
-        else:
-            ranking = Sprite("png/ranking.png")
-            ranking.set_position(janela.width/2-jogar.width/2, 3*janela.height/6)
-
-        #sair
-        if cursor.is_over_area([janela.width/2-jogar.width/2, 4*janela.height/6], [janela.width/2+jogar.width/2, 4*janela.height/6+jogar.height]):
-            sair = Sprite("png/sair-2.png")
-            sair.set_position(janela.width/2-jogar.width/2, 4*janela.height/6)
-            if cursor.is_button_pressed(1):
-                exit()
-        else:
-            sair = Sprite("png/sair.png")
-            sair.set_position(janela.width/2-jogar.width/2, 4*janela.height/6) 
-    
-    if telanum == 1 and teclado.key_pressed("esc"):
-        telanum = 0
-
-    if telanum == 0 :
+    if telanum == 0: #CASO ESTEJA NA TELA INICIAL, DESENHAR CADA BOTÃO COMO SPRITE
         jogar.draw()
         dificuldade.draw()
         ranking.draw()
         sair.draw()
 
-    if telanum == 2:
+    #TROCA DE SPRITE DO BOTÃO JOGAR DEPENDENDO DA POSIÇÃO DO CURSOR
+        if cursor.is_over_area([janela.width/2-jogar.width/2, janela.height/6], [janela.width/2+jogar.width/2, janela.height/6+jogar.height]):
+            jogar = Sprite("Space Invaders/png/jogar-2.png")
+            jogar.set_position(janela.width/2-jogar.width/2, janela.height/6)
+            if cursor.is_button_pressed(1):
+                janela.clear()
+                telanum = 1
+        else:
+            jogar = Sprite("Space Invaders/png/jogar.png")
+            jogar.set_position(janela.width/2-jogar.width/2, janela.height/6)
 
+        #TROCA DE SPRITE DO BOTÃO JOGAR DIFICULDADE DA POSIÇÃO DO CURSOR
+        if cursor.is_over_area([janela.width/2-jogar.width/2, 2*janela.height/6], [janela.width/2+jogar.width/2, 2*janela.height/6+jogar.height]):
+            dificuldade = Sprite("Space Invaders/png/dificuldade-2.png")
+            dificuldade.set_position(janela.width/2-jogar.width/2, 2*janela.height/6)
+            if cursor.is_button_pressed(1):
+                janela.clear()
+                telanum = 2
+        else:
+            dificuldade = Sprite("Space Invaders/png/dificuldade.png")
+            dificuldade.set_position(janela.width/2-jogar.width/2, 2*janela.height/6) 
+
+        #TROCA DE SPRITE DO BOTÃO RANKING DEPENDENDO DA POSIÇÃO DO CURSOR
+        if cursor.is_over_area([janela.width/2-jogar.width/2, 3*janela.height/6], [janela.width/2+jogar.width/2, 3*janela.height/6+jogar.height]):
+            ranking = Sprite("Space Invaders/png/ranking-2.png")
+            ranking.set_position(janela.width/2-jogar.width/2, 3*janela.height/6)
+        else:
+            ranking = Sprite("Space Invaders/png/ranking.png")
+            ranking.set_position(janela.width/2-jogar.width/2, 3*janela.height/6)
+
+       #TROCA DE SPRITE DO BOTÃO SAIR DEPENDENDO DA POSIÇÃO DO CURSOR
+        if cursor.is_over_area([janela.width/2-jogar.width/2, 4*janela.height/6], [janela.width/2+jogar.width/2, 4*janela.height/6+jogar.height]):
+            sair = Sprite("Space Invaders/png/sair-2.png")
+            sair.set_position(janela.width/2-jogar.width/2, 4*janela.height/6)
+            if cursor.is_button_pressed(1):
+                exit()
+        else:
+            sair = Sprite("Space Invaders/png/sair.png")
+            sair.set_position(janela.width/2-jogar.width/2, 4*janela.height/6)
+    
+    if telanum == 1 and teclado.key_pressed("esc"): #CASO ESTEJA NA TELA DO JOGO E APERTAR ESC, VOLTAR PRO MENU
+        telanum = 0
+
+
+    if telanum == 2: #IR PRA TELA DE ESCOLHA DE DIFICULDADE E DESENHAR OS BOTÕES FÁCIL, MÉDIO E DIFÍCIL
         facil.draw()
         medio.draw()
         dificil.draw()
 
+        #TROCA DE SPRITE DO BOTÃO FÁCIL DEPENDENDO DA POSIÇÃO DO CURSOR
         if cursor.is_over_area([janela.width/2-jogar.width/2, janela.height/6], [janela.width/2+jogar.width/2, janela.height/6+jogar.height]):
-            facil = Sprite("png/facil-2.png")
+            facil = Sprite("Space Invaders/png/facil-2.png")
             facil.set_position(janela.width/2-jogar.width/2, janela.height/6)
             if cursor.is_button_pressed(1):
                 janela.clear()
                 telanum = 1
         else:
-            facil = Sprite("png/facil.png")
+            facil = Sprite("Space Invaders/png/facil.png")
             facil.set_position(janela.width/2-jogar.width/2, janela.height/6)
 
+        #TROCA DE SPRITE DO BOTÃO MÉDIO DEPENDENDO DA POSIÇÃO DO CURSOR
         if cursor.is_over_area([janela.width/2-jogar.width/2, 3*janela.height/6], [janela.width/2+jogar.width/2, 3*janela.height/6+jogar.height]):
-            medio = Sprite("png/medio-2.png")
+            medio = Sprite("Space Invaders/png/medio-2.png")
             medio.set_position(janela.width/2-jogar.width/2, 3*janela.height/6)
             if cursor.is_button_pressed(1):
                 janela.clear()
                 telanum = 1
         else:
-            medio = Sprite("png/medio.png")
+            medio = Sprite("Space Invaders/png/medio.png")
             medio.set_position(janela.width/2-jogar.width/2, 3*janela.height/6)
 
+        #TROCA DO BOTÃO DIFÍCIL DEPENDENDO DA POSIÇÃO DO CURSOR
         if cursor.is_over_area([janela.width/2-jogar.width/2, 5*janela.height/6], [janela.width/2+jogar.width/2, 5*janela.height/6+jogar.height]):
-            dificil = Sprite("png/dificil-2.png")
+            dificil = Sprite("Space Invaders/png/dificil-2.png")
             dificil.set_position(janela.width/2-jogar.width/2, 5*janela.height/6)
             if cursor.is_button_pressed(1):
                 janela.clear()
                 telanum = 1
         else:
-            dificil = Sprite("png/dificil.png")
+            dificil = Sprite("Space Invaders/png/dificil.png")
             dificil.set_position(janela.width/2-jogar.width/2, 5*janela.height/6)
