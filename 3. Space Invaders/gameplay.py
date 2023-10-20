7a89ef14d6b021e379dc817da9ae73773b1bcb58
Bruno Porto <bpmonteiro@id.uff.br>
2023-10-20 00:32:23 -0300

    Tiros
    Atualizei o gameplay.py. Agora a nave dá mais de um tiro direito. Depois posso atualizar tamanho do tiro e cadência.

--- 3. Space Invaders/gameplay.py
+++ 3. Space Invaders/gameplay.py
@@ -2,7 +2,10 @@ from PPlay.window import *
 from PPlay.sprite import *
 from PPlay.gameimage import *
 
-disparos = []
+def atira():
+    tiro = Sprite("png/tiro.png")
+    tiro.set_position(nave.x+nave.width/2-2, nave.y)
+    return tiro
 
 janela = Window(500, 500)
 janela.set_title("Space Invaders")
@@ -14,26 +17,29 @@ nave = Sprite("png/pave.png")
 velnav = 700
 veltiro = -500
 
+timer = 0
+tiros = []
+
 nave.set_position(janela.width/2 - nave.width/2, 400-nave.height/4)
 
 while True:
-    
     fundo.draw()
     nave.draw()
-    janela.update()
-
+    timer += janela.delta_time()
     if teclado.key_pressed("left"):
         if nave.x + 2 > 0:
             nave.move_x(-velnav* janela.delta_time())
     if teclado.key_pressed("right"):
         if nave.x < janela.width - nave.width:
             nave.move_x(velnav* janela.delta_time())
-    if teclado.key_pressed("space"):
-        tiro = Sprite("png/tiro.png", 5)
-        tiro.set_sequence_time(0, 4, 300, True)
-        tiro.set_position(nave.x+nave.width/2-2, nave.y)
-        disparos.append(tiro)
-    if disparos != []:
-        for d in disparos:
-            d.draw()
-            d.y += veltiro* janela.delta_time()
+    if teclado.key_pressed("space") and timer > 0.1:
+        tiros.append(atira())
+        timer = 0     
+    if tiros != []:
+        for t in tiros:
+            t.draw()
+            t.y += veltiro* janela.delta_time()
+            if t.y < 0:
+                tiros.remove(t)
+    tiro.y += veltiro* janela.delta_time()
+    janela.update()
