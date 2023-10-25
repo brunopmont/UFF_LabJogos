308f8232db19fde783da7d98c0d2f5dbebd910e9
Bruno Porto <bpmonteiro@id.uff.br>
2023-10-25 04:12:27 -0300

    Versão alpha com zumbi
    Meti um mob com ia meia boca, mas tá valendo

--- 4. Vampiro Carioca/alpha.py
+++ 4. Vampiro Carioca/alpha.py
@@ -10,30 +10,37 @@ player = Sprite("4. Vampiro Carioca/png/player.png")
 esqueleto = Sprite("4. Vampiro Carioca/png/esqueleto.png")
 policial = Sprite("4. Vampiro Carioca/png/policial.png")
 zumbi = Sprite("4. Vampiro Carioca/png/zumbi.png")
-player.set_position(janela.width/2-player.width/2, janela.height/2-player.height/2)
-esqueleto.set_position(janela.width/2-player.width/2, janela.height/2-player.height/2)
-policial.set_position(janela.width/2-player.width/2, janela.height/2-player.height/2)
-zumbi.set_position(janela.width/2-player.width/2, janela.height/2-player.height/2)
+player.set_position(460, 460)
+esqueleto.set_position(460, 10)
+policial.set_position(10, 460)
+zumbi.set_position(10, 10)
 
 velplayer = 300
+velmob = 150
 
 while True:
     if teclado.key_pressed("w"):
         if player.y - 1 > 0: 
             player.move_y(janela.delta_time() * -velplayer)
-
     if teclado.key_pressed("s"):
         if player.y + 1 < janela.height - player.height: 
             player.move_y(janela.delta_time() * velplayer)
-
     if teclado.key_pressed("a"):
         if player.x - 1 > 0: 
             player.move_x(janela.delta_time() * -velplayer)
-
     if teclado.key_pressed("d"):
         if player.x + 1 < janela.width - player.width: 
             player.move_x(janela.delta_time() * velplayer)
 
+    if zumbi.x > player.x:
+        zumbi.move_x(janela.delta_time() * -velmob)
+    else:
+        zumbi.move_x(janela.delta_time() * velmob)
+    if zumbi.y > player.y:
+        zumbi.move_y(janela.delta_time() * -velmob)
+    else:
+        zumbi.move_y(janela.delta_time() * velmob)
     janela.update()
     fundo.draw()
     player.draw()
+    zumbi.draw()
